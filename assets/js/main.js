e4a4ae4d8b94ea328097e95b0fc642ae1c61e7b3
Bodruzzaman-Khan <bodruzzamankhan.sau@gmail.com>
2024-10-26 01:37:38 +0600

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Editorial by HTML5 UP
+	Bodruzzaman Khan
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -234,15 +234,6 @@
 
 				});
 
-
-
-
-	// Get the last updated date
-		const lastUpdated = new Date(document.lastModified);
-		const options = { year: 'numeric', month: 'long', day: 'numeric' };
-		document.getElementById('last-update').innerText = lastUpdated.toLocaleDateString(undefined, options);
-
-
 	// Menu.
 		var $menu = $('#menu'),
 			$menu_openers = $menu.children('ul').find('.opener');
@@ -267,5 +258,12 @@
 				});
 
 			});
+	// Get the last updated date
+		const lastUpdated = new Date(document.lastModified);
+		const options = { year: 'numeric', month: 'long', day: 'numeric' };
+		document.getElementById('last-update').innerText = lastUpdated.toLocaleDateString(undefined, options);
 
+
+
+	
 })(jQuery);
